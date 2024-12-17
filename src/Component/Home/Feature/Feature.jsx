0f6f730f48ff8f1a0673427d4d82923c86b38e35
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-12-18 00:54:11 +0600

    Feature Section Done

--- src/Component/Home/Feature/Feature.jsx
+++ src/Component/Home/Feature/Feature.jsx
@@ -1,8 +1,33 @@
 
+const featureIcon = [
+    { logo: "https://i.postimg.cc/h468W23B/SVG.png" },
+    {  logo: "https://i.postimg.cc/RVt7Mwjn/SVG-1.png" },
+    {  logo: "https://i.postimg.cc/g0gRMs61/SVG-5.png" },
+    {  logo: "https://i.postimg.cc/fRhxkJNq/SVG-2.png" },
+    {  logo: "https://i.postimg.cc/bwH1npYy/SVG-3.png" },
+    {  logo: "https://i.postimg.cc/rpVxC3zS/SVG-4.png" },
+ 
+]
+
+
 const Feature = () => {
     return (
-        <div>
-            <h1>Feature</h1>
+        <div className="mx-auto container p-4 md:p-2">
+            <div className="flex items-center">
+                <h1 className="text-[#7D89B0]">Feature in Trusted by</h1>
+                <img src={'https://i.postimg.cc/WpWWDmWW/arrow-right-02.png'} alt="" className="ml-2" />
+            </div>
+
+            <div className="flex justify-between mt-4">
+                    {featureIcon.map((skill, index) => (
+                        <div
+                            key={index}
+                            className=""
+                        >
+                            <img src={skill.logo} alt={'not Found'}  />
+                        </div>
+                    ))}
+                </div>
         </div>
     );
 };
